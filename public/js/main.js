c970357c70612cc7c77a3e91f37fca4e528311ba
R. Caner YILDIRIM <caner@Caners-MacBook-Pro.local>
2020-02-10 21:34:14 +0300

    mobile menu changes started

--- public/js/main.js
+++ public/js/main.js
@@ -17,6 +17,22 @@ $(window).on('scroll', function () {
 
 
 $(document).ready(function(){
+  var isLateralNavAnimating = false;
+    
+    //open/close lateral navigation
+    $('.cd-nav-trigger').on('click', function(event){
+        event.preventDefault();
+        //stop if nav animation is running 
+        if( !isLateralNavAnimating ) {
+            if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+            
+            $('body').toggleClass('navigation-is-open');
+            $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+                //animation is over
+                isLateralNavAnimating = false;
+            });
+        }
+    });
 
 // mobile_menu
 var menu = $('ul#navigation');
